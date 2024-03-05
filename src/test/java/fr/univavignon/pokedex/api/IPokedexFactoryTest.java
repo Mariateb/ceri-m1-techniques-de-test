ee8233adaaaab2b128681aed30ab527a5d09e478
Mariateb <sabmou05@gmail.com>
2024-03-05 09:24:35 +0100

    Ajout test PokedexFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -1,4 +1,35 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Comparator;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
 public class IPokedexFactoryTest {
+
+    @Mock
+    private IPokedexFactory factory;
+
+    @Mock
+    private IPokemonMetadataProvider metadataProvider;
+
+    @Mock
+    private IPokemonFactory pokemonFactory;
+
+    @Test
+    public void testPokedexCreation() {
+        metadataProvider = Mockito.mock(IPokemonMetadataProvider.class);
+        pokemonFactory = Mockito.mock(IPokemonFactory.class);
+
+        factory = Mockito.mock(IPokedexFactory.class);
+
+        IPokedex pokedex = factory.createPokedex(metadataProvider, pokemonFactory);
+        assertNotNull(pokedex);
+    }
 }
