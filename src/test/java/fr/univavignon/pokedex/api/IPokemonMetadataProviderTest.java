2cf1293c7ab6827eb8f6ad33105163e426987c44
Mariateb <sabmou05@gmail.com>
2024-03-05 08:54:04 +0100

    Ajout test PokemonMetadataProvider

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,4 +1,28 @@
 package fr.univavignon.pokedex.api;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
 public class IPokemonMetadataProviderTest {
+    @Test
+    public void testProvide() throws PokedexException {
+        IPokemonMetadataProvider provider;
+
+        provider = Mockito.mock(IPokemonMetadataProvider.class);
+        int aqualiIndex = 133;
+
+        PokemonMetadata expected = new PokemonMetadata(
+                133,
+                "Aquali",
+                186,
+                168,
+                260
+        );
+        
+        when(provider.getPokemonMetadata(aqualiIndex)).thenReturn(expected);
+        assertEquals(expected, provider.getPokemonMetadata(aqualiIndex));
+    }
 }
