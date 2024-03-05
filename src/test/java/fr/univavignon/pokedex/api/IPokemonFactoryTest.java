e72b084a5fca14854a15fe395f52f6f2f75cc909
Mariateb <sabmou05@gmail.com>
2024-03-05 08:59:50 +0100

    Ajout test PokemonFactory

--- src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonFactoryTest.java
@@ -1,4 +1,43 @@
 package fr.univavignon.pokedex.api;
 
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
 public class IPokemonFactoryTest {
+    @Test
+    public void testPokemonCreation() throws PokedexException {
+        IPokemonFactory factory;
+
+        factory = Mockito.mock(IPokemonFactory.class);
+
+        int aqualiIndex = 133;
+
+        Pokemon expected = new Pokemon(
+                133,
+                "Aquali",
+                186,
+                168,
+                260,
+                2729,
+                202,
+                5000,
+                4,
+                100
+        );
+
+        when(factory.createPokemon(133, 2729, 202, 5000, 4)).thenReturn(expected);
+        Pokemon actual = factory.createPokemon(133, 2729, 202, 5000, 4);
+        assertEquals(expected.getIndex(), actual.getIndex());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getAttack(), actual.getAttack());
+        assertEquals(expected.getDefense(), actual.getDefense());
+        assertEquals(expected.getStamina(), actual.getStamina());
+        assertEquals(expected.getCp(), actual.getCp());
+        assertEquals(expected.getHp(), actual.getHp());
+        assertEquals(expected.getDust(), actual.getDust());
+        assertEquals(expected.getCandy(), actual.getCandy());
+    }
 }
