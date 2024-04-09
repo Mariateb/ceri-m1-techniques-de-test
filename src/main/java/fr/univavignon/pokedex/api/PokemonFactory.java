ab152ae569da754b62c13fcec67c34ceeac66084
Mariateb <sabmou05@gmail.com>
2024-04-09 11:18:22 +0200

    Ajout fichier team rocket

--- src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/PokemonFactory.java
@@ -1,32 +1,60 @@
 package fr.univavignon.pokedex.api;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
+
+import fr.univavignon.pokedex.api.IPokemonFactory;
+import fr.univavignon.pokedex.api.Pokemon;
+
 public class PokemonFactory implements IPokemonFactory {
+
+    private static final Map<Integer, String> index2name;
+    static {
+        Map<Integer, String> aMap = new HashMap<Integer, String>();
+        aMap.put(-1, "Ash's Pikachu");
+        aMap.put(0, "MISSINGNO");
+        aMap.put(1, "Bulbasaur");
+        //TODO : Gotta map them all !
+        index2name = new HashMap<Integer, String>(aMap);
+    }
+
+    private static int generateRandomStat() {
+        int total = 0;
+        for(int i=0; i < 1000000; i++)
+        {
+            Random rn = new Random();
+            int r = rn.nextInt(2);
+            total = total + r;
+        }
+        return total / 10000;
+    }
+
     @Override
     public Pokemon createPokemon(int index, int cp, int hp, int dust, int candy) {
-        PokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
-        PokemonMetadata metadata;
-
-        try {
-            metadata = metadataProvider.getPokemonMetadata(index);
-        } catch (PokedexException e) {
-            return null;
+        String name;
+        if(!index2name.containsKey(index)) {
+            name = index2name.get(0);
+        } else {
+            name = index2name.get(index);
         }
-
-        Random random = new Random();
-
-        return new Pokemon(
-                index,
-                metadata.getName(),
-                metadata.getAttack(),
-                metadata.getDefense(),
-                metadata.getStamina(),
-                cp,
-                hp,
-                dust,
-                candy,
-                random.nextInt(32)
-        );
+        int attack;
+        int defense;
+        int stamina;
+        double iv;
+        if(index < 0) {
+            attack = 1000;
+            defense = 1000;
+            stamina = 1000;
+            iv = 0;
+        } else {
+            attack = PokemonFactory.generateRandomStat();
+            defense = PokemonFactory.generateRandomStat();
+            stamina = PokemonFactory.generateRandomStat();
+            iv = 1;
+        }
+        return new Pokemon(index, name, attack, defense, stamina, cp, hp, dust, candy, iv);
     }
+
 }
